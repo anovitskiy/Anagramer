cdb56e565f8c3d8ed9f2dfa0a59446be54d4e102
Alan Novitskiy <anovitskiy+github@gmail.com>
2014-07-07 09:20:28 -0700

    add simple test for main application

---
+++ test/AnagramerTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class AnagramerTest {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStream() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStream() {
+	    System.setOut(null);
+	}
+
+	@Test
+	public void testMain() {
+		Anagramer.main(null);
+		assertEquals("FALSE\nFALSE\nFALSE\nFALSE\nFALSE\nFALSE\nTRUE\nFALSE\nFALSE\nTRUE\nFALSE\nTRUE\nFALSE\n",outContent.toString());
+	}
+}
